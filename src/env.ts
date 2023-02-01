1fe766a0de54d70819021a973fb0ce44860647d4
Piper McCorkle <contact@piperswe.me>
2023-02-01 16:45:32 -0600

    Redirect root to cfaccess

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 export default interface Env {
   LOGIN_ANALYTICS: AnalyticsEngineDataset;
   DOMAIN: string;
+  ROOT_REDIRECT: string;
   NAME: string;
   AUTHCODES: KVNamespace;
   CSRF_TOKENS: KVNamespace;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { WorkerRouter } from "@worker-tools/router";
-import { ok } from "@worker-tools/response-creators";
+import { permanentRedirect } from "@worker-tools/response-creators";
 import oidcDiscovery from "./oidc/discovery";
 import { jwksClearEndpoint, jwksEndpoint } from "./jwks/endpoint";
 import { authorizationForm, authorizationEndpoint } from "./oidc/authorization";
 import admin from "./admin";
 import tokenEndpoint from "./oidc/token";
+import Env from "./env";
 
 export default new WorkerRouter()
-  .get("/", () => ok("hello, world"))
+  .get("/", (req, ctx) => permanentRedirect((ctx.env as Env).ROOT_REDIRECT))
   .get("/.well-known/openid-configuration", oidcDiscovery)
   .get("/.well-known/jwks.json", jwksEndpoint)
   .delete("/.well-known/jwks.json", jwksClearEndpoint)
